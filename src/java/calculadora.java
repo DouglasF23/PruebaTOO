36872ffffcf4439b57704bdadd342fd1ad667be3
Douglas <Douglas@Douglas>
2014-10-14 16:18:56 -0600

    Tercera  version

--- src/java/calculadora.java
+++ src/java/calculadora.java
@@ -9,6 +9,9 @@
  * @author Douglas
  */
 public class calculadora {
-   
+    public final int suma=1;
+    public final int resta=2;
+    public final int multiplicacion=3;
+    public final int division=4;
     
 }
